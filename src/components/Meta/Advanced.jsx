b95513fb21673038619db2559bba3fc820bc8073
Luigi Cruz <luigifcruz@icloud.com>
2019-04-20 15:54:20 -0300

    first commit

---
+++ src/components/Meta/Advanced.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Advanced extends Component {
+	render() {
+		return (
+			<div>
+				Advanced options will be put here.
+			</div>
+        );
+	}
+}
+
+export default Advanced
